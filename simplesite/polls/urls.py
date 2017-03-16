f72ea1644b4c67c9afc07700e3e874d03972469a
Tana Jukes <tana.jukes@gmail.com>
2017-03-16 14:21:01 -0700

    Placeholder view to vote on a question

--- simplesite/polls/urls.py
+++ simplesite/polls/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
     # eg: /polls/4/results/
     url(r'^(?P<question_id>[0-9]+)/results/', views.results, name="results"),
+    # eg: /polls/4/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote"),
 ]
--- simplesite/polls/views.py
+++ simplesite/polls/views.py
@@ -10,3 +10,6 @@ def detail(request, question_id):
 def results(request, question_id):
     response = "Results for question %s."
     return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Vote on question %s." % question_id)
