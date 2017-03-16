6b52548950ee1d018c21fe97096dc77403246be6
Tana Jukes <tana.jukes@gmail.com>
2017-03-16 15:15:24 -0700

    Add 404 if question not found (detail view)
    Import Django's 'Http404' class and use it to return a custom message if no question is found for the ID passed in to the question detail view.

--- simplesite/polls/views.py
+++ simplesite/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 
 from .models import Question
 
@@ -11,6 +12,10 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
