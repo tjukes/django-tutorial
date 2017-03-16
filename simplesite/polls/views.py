f6f30eb5fd31ae6e08e9f3856b3f4af08c92b094
Tana Jukes <tana.jukes@gmail.com>
2017-03-16 14:31:53 -0700

    Make Question available to views
    Import the Question model into the view file, so that different views can access info about questions (details from the db table).

--- simplesite/polls/views.py
+++ simplesite/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
     return HttpResponse("Hello, world. This is the polls index.")
 
