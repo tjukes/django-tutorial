f146e3cbaa0c1caf4dd7bf061b3668cd5c87606f
Tana Jukes <tana.jukes@gmail.com>
2017-03-16 14:33:14 -0700

    Show recent questions in index view
    Basic wiring up of index view so that it displays the most recent five questions instead of a Hello World message.

--- simplesite/polls/views.py
+++ simplesite/polls/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. This is the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ", ".join(q.question_text for q in latest_question_list)
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("This is question %s." % question_id)
