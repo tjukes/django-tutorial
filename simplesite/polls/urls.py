2664c66f1404a2ba93415ea5bfd983e093969332
Tana Jukes <tana.jukes@gmail.com>
2017-03-16 13:43:18 -0700

    Placeholder view for single question
    Created a simple view that takes a question ID as an argument and displays a simple string with the question ID in it. Added a URL matcher so that requests to 'polls/id' will display the relevant view for the question whose ID is passed in.

--- simplesite/polls/urls.py
+++ simplesite/polls/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    # eg: /polls/4/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
 ]
--- simplesite/polls/views.py
+++ simplesite/polls/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. This is the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("This is question %s." % question_id)
